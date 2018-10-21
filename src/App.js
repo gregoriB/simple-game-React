5f04617622ecf9f0f2ee75645c9b1470c3644f74
gregoriB <brandon.gregori@gmail.com>
2018-10-21 09:47:02 -0700

    basic movment and map boudaries in place

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    playerPos: [0, 0]
+  }
+
+  playerSize = 20;
+  mapHeight = this.playerSize * 20;
+  mapWidth = this.playerSize * 40;
+
+  handlePlayerMove = (index, value) => {
+    const newPlayerPos = [...this.state.playerPos];
+    const axis = index === 0 ? 0 : 1;
+    const boundary = index === 0 ? this.mapWidth : this.mapHeight;
+    console.log(axis)
+    newPlayerPos.splice(index, 1, newPlayerPos[index] + value)
+    if (newPlayerPos[axis] < 0 || newPlayerPos[axis] >= (boundary)) {
+      return;
+    }
+    this.setState(() => ({ playerPos: newPlayerPos }))
+  }
+
+  handleMove = (e) => {
+    e.preventDefault();
+    switch(e.keyCode) {
+      case 37: //left
+        return this.handlePlayerMove(0, -20)
+      case 38: //up
+        return this.handlePlayerMove(1, -20)
+      case 39: //right
+        return this.handlePlayerMove(0, 20)
+      case 40: //down
+        return this.handlePlayerMove(1, 20)
+      default:
+        console.log(e.keyCode);
+    }
+  }
+  
+  componentDidMount() {
+    document.addEventListener('keydown', (e) => this.handleMove(e));
+    console.log(this.initialPos)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', (e) => this.handleMove(e));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='map'>
+          <div 
+            className='player'
+            style={{
+              position: 'relative',
+              top: this.state.playerPos[1],
+              left: this.state.playerPos[0]
+
+            }}
+          />
       </div>
     );
   }
