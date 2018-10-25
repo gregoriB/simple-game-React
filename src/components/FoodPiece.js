d297626d0620f8398d5cc33dab362ede248efd06
gregoriB <brandon.gregori@gmail.com>
2018-10-24 21:45:03 -0700

    small changes to FoodPiece.js

--- src/components/FoodPiece.js
+++ src/components/FoodPiece.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class FoodPiece extends Component {
-
-  render() {
-    return (
-      <div 
-        className='food'
-        style={{
-          background: this.props.color,
-          top: this.props.foodY,
-          left: this.props.foodX,
-          padding: this.props.foodSize,
-        }}
-      />
-    )
-  }
+const FoodPiece = (props) => {
+  return (
+    <div 
+      className='food'
+      style={{
+        background: props.color,
+        top: props.foodY,
+        left: props.foodX,
+        padding: props.foodSize,
+      }}
+    />
+  )
 }
+
+export default FoodPiece;
